0cf26b62100bd68b58c6cc8767dbb76ab1accc83
thamiris.monteiro <thamirismylius@gmail.com>
2020-09-07 12:15:26 -0300

    Grades API

---
+++ controllers/gradeController.js
@@ -0,0 +1,127 @@
+import { db } from '../models/index.js';
+import { logger } from '../config/logger.js';
+
+const Grade = db.grade;
+
+const create = async (req, res) => {
+  const grade = new Grade({
+    name: req.body.name,
+    subject: req.body.subject,
+    type: req.body.type,
+    value: req.body.value,
+    lastModified: req.body.lastModified,
+  });
+  try {
+    const data = await grade.save();
+    res.send(data);
+    res.send({ message: 'Grade inserido com sucesso' });
+    logger.info(`POST /grade - ${JSON.stringify()}`);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || 'Algum erro ocorreu ao salvar' });
+    logger.error(`POST /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const findAll = async (req, res) => {
+  const name = req.query.name;
+
+  //condicao para o filtro no findAll
+  var condition = name
+    ? { name: { $regex: new RegExp(name), $options: 'i' } }
+    : {};
+
+  try {
+    const data = await Grade.find(condition);
+    if (!data) {
+      res.status(400).send({ message: 'Erro ao listar todos os documentos' });
+    } else {
+      res.send(data);
+    }
+    logger.info(`GET /grade`);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || 'Erro ao listar todos os documentos' });
+    logger.error(`GET /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const findOne = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const data = await Grade.findById({ _id: id });
+    if (!data) {
+      res.status(400).send({ message: 'Erro ao buscar o Documento id: ' + id });
+    } else {
+      res.send(data);
+    }
+    logger.info(`GET /grade - ${id}`);
+  } catch (error) {
+    res.status(500).send({ message: 'Erro ao buscar o Grade id: ' + id });
+    logger.error(`GET /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const update = async (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: 'Dados para atualizacao vazio',
+    });
+  }
+
+  const id = req.params.id;
+
+  try {
+    Pokemon.findByIdAndUpdate({ _id: id }, req.body, { new: true });
+    if (!data) {
+      res.status(400).send({ message: 'Erro ao atualizar a Grade id: ' + id });
+    } else {
+      res.send(data);
+    }
+    logger.info(`PUT /grade - ${id} - ${JSON.stringify(req.body)}`);
+  } catch (error) {
+    res.status(500).send({ message: 'Erro ao atualizar a Grade id: ' + id });
+    logger.error(`PUT /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const remove = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    logger.info(`DELETE /grade - ${id}`);
+    const data = await Pokemon.findByIdAndRemove({ _id: id });
+    if (!data) {
+      res
+        .status(400)
+        .send({ message: 'Nao foi possivel deletar o Grade id: ' + id });
+    } else {
+      res.send({ message: 'Pokemon excluido com sucesso' });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: 'Nao foi possivel deletar o Grade id: ' + id });
+    logger.error(`DELETE /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const removeAll = async (req, res) => {
+  try {
+    const data = Grade.deleteMany();
+    if (!data) {
+      res.status(400).send({ message: 'Erro ao excluir todos os Grades' });
+    } else {
+      res.send({ message: 'Grades excluidos com sucesso' });
+    }
+    logger.info(`DELETE /grade`);
+  } catch (error) {
+    res.status(500).send({ message: 'Erro ao excluir todos as Grades' });
+    logger.error(`DELETE /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+export default { create, findAll, findOne, update, remove, removeAll };
